51cd03dbe91fa1c31a8cef30315c2d0c7d248538
Max Dobeck <msdobeck@gmail.com>
2018-05-13 21:30:23 -0700

    Added a test based on examples from golang's httptest package.

--- tests/login_test.go
+++ tests/login_test.go
@@ -1,11 +1,31 @@
 package main
 
 import (
-    "net/http"
-    "net/http/httptest"
-    "testing"
+	"fmt"
+	"github.com/maxdobeck/gatekeeper/authentication"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
+// Consider adding to golang examples for httptest
 func TestLogin(t *testing.T) {
+	bodyReader := strings.NewReader(`{"email": "WrongEmail@email.com", "password": "wrongPassword"}`)
+	req, err := http.NewRequest("POST", "/login", bodyReader)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	req := httptest.NewRequest("GET", "http://example.com/foo", nil)
+	w := httptest.NewRecorder()
+	gatekeeper.login(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(string(body))
 }
